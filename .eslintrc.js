a71526f953a639157b02042dc69f46c766af90c3
muhab alkayale <Muhabalkayale@gmail.com>
2024-09-27 09:26:15 -0300

    FREAK BOB

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-empty-interface": "off",
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
